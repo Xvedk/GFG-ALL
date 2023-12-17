bf91d673b750558725f268e6651f68643113b31c
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2023-12-17 16:01:32 +0530

    Create 17-12-2023.py

---
+++ POTD/17-12-2023.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMaxSum(self, arr, n):
+        if n == 0:
+            return 0
+        
+        incl = arr[0]
+        excl = 0
+        
+        for i in range(1, n):
+            new_excl = max(incl, excl)
+            incl = excl + arr[i]
+            excl = new_excl
+            
+        return max(incl, excl)
