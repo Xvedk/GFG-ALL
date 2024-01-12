6dfbd4eb254688658652dcafbd83c570f4e9e8ba
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2024-01-12 17:07:47 +0530

    Create 12-01-2024.cpp

---
+++ POTD/2024/JANUARY/12-01-2024.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+    
+    // Function to reverse first k elements of a queue.
+    queue<int> modifyQueue(queue<int> q, int k) {
+        // add code here.
+        stack<int> s;
+        for(int i=0;i<k;i++) 
+        {
+            s.push(q.front());
+            q.pop();
+        }
+        while(s.size()!=0)
+        {
+            q.push(s.top());
+            s.pop();
+        }
+        for(int i=0;i<(q.size()-k);i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        return q;
+    }
+    };
