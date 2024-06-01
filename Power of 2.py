e6e7f68ccba5c993fa0e3e2c4bf0cc7cf8056f8f
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2024-06-01 19:57:35 +0530

    Create Power of 2.py

---
+++ Power of 2.py
@@ -0,0 +1,9 @@
+class Solution:
+    ##Complete this function
+    # Function to check if given number n is a power of two.
+    def isPowerofTwo(self,n : int ) -> bool:
+        if n== 0:
+            return False
+        if (n&(n-1)) == 0:
+            return True
+        return False
