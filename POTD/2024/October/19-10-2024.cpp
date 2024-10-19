7c42ce75058009657398d6ec2f560d32a9bec462
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2024-10-19 20:34:25 +0530

    Create 19-10-2024.cpp

---
+++ POTD/2024/October/19-10-2024.cpp
@@ -0,0 +1,33 @@
+string roundToNearest(string str) {
+        int n = str.size();
+        
+        // base case
+        if(str[n-1] == '0') 
+            return str;
+            
+        // 0 < last digit < 6
+        else if('0' < str[n-1] && str[n-1] < '6'){
+            str.pop_back();
+            str.push_back('0');
+            return str;
+        }
+        
+        // else, 5 < last digit <= 9
+        str.pop_back();
+        str.push_back('0');
+        int i = n-2;
+        while(str[i]=='9' && i>=0){
+            str[i] = '0';
+            i--;
+        }
+        // if all are '9' ex. 99997
+        if(i<0){ 
+            return '1'+str;
+        }    
+        
+        else{ // like example: 1239998
+            str[i] = (str[i]+1);
+        }
+        
+        return str;        
+    }
