3c62af015b8288c9614294b96480c8997d71b560
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2024-11-09 09:10:24 +0530

    Update 09-11-2024.py

--- POTD/2024/November/09-11-2024.py
+++ POTD/2024/November/09-11-2024.py
@@ -1,16 +1,71 @@
-class Solution:
-    def minEnd(self, n: int, x: int) -> int:
-        X = x
-        N = n - 1
-        ans = 0
-        j = 0
+class Solution {
+  public:
+    string minSum(vector<int> &arr) {
+        
+        int n=arr.size();
+     sort(arr.begin(),arr.end());
+        vector<int>v1;
+        for(int i=0;i<n;i++){
+            if(i%2!=0){
+                v1.push_back(arr[i]);
+                
+            }
+        }
+           vector<int>v2;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                v2.push_back(arr[i]);
+               
+                
+            }
+        }
+        
+        reverse(v1.begin(),v1.end());
+        reverse(v2.begin(),v2.end());
+        vector<int>ans;
+        int i=0;
+        int j=0;
+        int carry=0;
+        while(i<v1.size() && j<v2.size()){
+            long long sum=v1[i]+v2[j]+carry;
+            int digit=sum%10;
+            ans.push_back(digit);
+            carry=sum/10;
+            i++;
+            j++;
+        }
+        while(i<v1.size()){
+            long long sum=v1[i]+carry;
+            int digit=sum%10;
+            ans.push_back(digit);
+            carry=sum/10;
+            i++;
+         
+        }
+            while(j<v2.size()){
+            long long sum=v2[j]+carry;
+            int digit=sum%10;
+            ans.push_back(digit);
+            carry=sum/10;
+            
+            j++;
+        }
+        if(carry!=0){
+            ans.push_back(carry);
+        }
+        reverse(ans.begin(),ans.end());
+     
+       string answer = "";
 
-        for i in range(56):  # Limit to 56 bits, as in the original C++ code
-            if (X >> i) & 1:  # Check if the i-th bit in X is set
-                ans |= (1 << i)  # Set the i-th bit in ans to 1
-            else:
-                if (N >> j) & 1:  # Use bits from N sequentially
-                    ans |= (1 << i)  # Set the i-th bit in ans according to N
-                j += 1  # Move to the next bit in N only if X[i] was not set
+        int index = 0;
+        while (index < ans.size() - 1 && ans[index] == 0) {
+            index++;
+        }
 
-        return ans
+        for (int i = index; i < ans.size(); i++) {
+            answer += to_string(ans[i]);
+        }
+
+        return answer.empty() ? "0" : answer;
+    }
+};
