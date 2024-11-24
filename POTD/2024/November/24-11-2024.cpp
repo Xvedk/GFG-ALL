6db99b813a062c3489cbe5836a91a510e7da831a
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2024-11-24 07:53:29 +0530

    Update and rename 24-11-2024.py to 24-11-2024.cpp

---
+++ POTD/2024/November/24-11-2024.cpp
@@ -0,0 +1,14 @@
+int maxSubarraySum(vector<int> &arr) {
+        int n = arr.size();
+        
+        int maxSum = INT_MIN, currSum = 0;
+        
+        for(int i = 0; i < n; ++i){
+            currSum += arr[i];
+            maxSum = max(maxSum, currSum);
+            
+            if(currSum < 0) currSum = 0;
+        }
+        
+        return maxSum;
+    }
