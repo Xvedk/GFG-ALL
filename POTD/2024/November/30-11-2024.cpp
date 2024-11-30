9aabbf6b1c33bc6fb2426cf105e2d84b5fbe704a
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2024-11-30 10:09:46 +0530

    Create 30-11-2024.cpp

---
+++ POTD/2024/November/30-11-2024.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    // Function is to check whether two strings are anagram of each other or not.
+    bool areAnagrams(string& s1, string& s2) {
+    if(s1.length()!=s2.length())
+    return false;
+    
+    int count[256]={0};
+    for(int i=0;i<s1.length();i++)
+    {
+        count[s1[i]]++;
+        count[s2[i]]--;
+    }
+    for(int i=0;i<256;i++)
+    {
+        if(count[i]!=0)
+        return false;
+    }
+        return true;
+    }
+};
+
