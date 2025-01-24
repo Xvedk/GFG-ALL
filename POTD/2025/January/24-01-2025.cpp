04e7f29aaa84905dd76fd6d76b8e861e79c05950
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2025-01-24 18:39:16 +0530

    Create 24-01-2025.cpp

---
+++ POTD/2025/January/24-01-2025.cpp
@@ -0,0 +1,17 @@
+bool detectLoop(Node* head) {
+        // your code here
+        if (head == NULL) return false;
+        
+        Node* fast = head, *slow = head;
+        
+        while (fast && fast -> next) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            
+            if (fast == slow) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
