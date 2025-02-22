4493311ee653763ee045f4b151f3480ae62e6767
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2025-02-22 17:20:44 +0530

    Create 22-02-2025.cpp

---
+++ POTD/2025/February/22-02-2025.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int maxLength(string& s) {
+        // code here
+        int n=s.size(),ans=0;
+        vector<int> dp(n+1,0);
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(') {
+                st.push(i);
+                continue;
+            }
+            if(st.empty()) continue;
+            int j=st.top(); st.pop();
+            dp[i]=i-j+1;
+            if(j>=1) dp[i]+=dp[j-1];
+            ans=max(ans,dp[i]);
+        }
+        return ans;
+    }
+    
+};
